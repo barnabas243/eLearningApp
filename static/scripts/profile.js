bc57fc69510a9c03b4e79ab4cbc1905455af7b79
barnabas Tan <bttf243@gmail.com>
2024-02-16 14:20:36 +0800

    added https for nginx. configured redis and celery image added webrtc app folder

--- static/scripts/profile.js
+++
@@ -1,6 +0,0 @@
-const uploadPictureModal = document.querySelector('#uploadPictureModal');
-
-uploadPictureModal.addEventListener('show.bs.modal', function (e) {
-    const btn = e.relatedTarget;
-    const imgUrl = btn.getAttribute('data-bs-imgUrl');
-})
